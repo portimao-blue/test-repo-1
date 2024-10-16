6bac8b157f4363e2bbc953d8b8e251df59b117c8
Kaviyarasu-S007 <kaviyarasushanthi2002@gmail.com>
2024-10-16 10:45:36 +0530

    Kavi's Update

--- movielens/ratings-counter.py
+++ movielens/ratings-counter.py
@@ -1,13 +1,20 @@
-from pyspark import SparkConf, SparkContext
-import collections
+# from pyspark import SparkConf, SparkContext
+# import collections
 
-conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
-sc = SparkContext(conf = conf)
+# conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+# sc = SparkContext(conf = conf)
 
-lines = sc.textFile("file:///SparkCourse/movielens/data/u.data")
-ratings = lines.map(lambda x: x.split()[2])
-result = ratings.countByValue()
+# lines = sc.textFile("file:///SparkCourse/movielens/data/u.data")
+# ratings = lines.map(lambda x: x.split()[2])
+# result = ratings.countByValue()
 
-sortedResults = collections.OrderedDict(sorted(result.items()))
-for key, value in sortedResults.items():
-    print("%s %i" % (key, value))
+# sortedResults = collections.OrderedDict(sorted(result.items()))
+# for key, value in sortedResults.items():
+#     print("%s %i" % (key, value))
+
+
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
+
+print("Kaviyarasu 😎")
